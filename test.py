1c5ac0f78e0d9fa81b9348616bfbe8efe244ad99
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-15 13:09:49 +0300

    created a users unnittest

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
+import unittest # Importing the unittest module
+from user import User  # Importing the user class
 
+
+class TestUser(unittest.TestCase):
+
+        '''
+        Test class that defines test cases for the user class behaviours.
+
+        Args:
+                unittest.TestCase: TestCase class that helps in creating test cases
+        '''
+        def setUp(self):
+                '''
+                Set up method to run before each test cases.
+                '''
+                self.new_user = User("Edwike","Nyash123") # create user object  
+
+        def test_init(self):
+                """
+                test_init test case to test if the object is initialized properly
+                """
+                self.assertEqual(self.new_user.username,"Edwike")
+                self.assertEqual(self.new_user.password,"Nyash123")
+
+
+
+if __name__ == '__main__':
+    unittest.main()                
     
