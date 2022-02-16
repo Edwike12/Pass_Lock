8ba6231a2f82b7e78e0ff22764071b178bd63073
Edwike12 <edwike.nyauncho@student.moringaschool.com>
2022-02-16 21:36:28 +0300

    add delete,display and locate credentials on credentials.py file

--- credentials.py
+++ credentials.py
@@ -20,6 +20,29 @@ class Credentials():
 
     def save_credentials(self):
             '''
-            method to store new credentials
+            method to save new credentials
             '''
-            Credentials.credentials_list.append(self)    
+            Credentials.credentials_list.append(self)   
+
+    def delete_credentials(self):
+        '''
+        Delete a credential from the list using this method.
+        '''
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def display_credentials(cls):
+        """
+        The credential array is returned by this function.
+        """
+        return cls.credentials_list
+
+
+    @classmethod
+    def locate_account(cls, account):
+        '''
+        A method that accepts a account name and returns the credentials for that site.
+        '''
+        for credentials in cls.credentials_list:
+            if credentials.account == account:
+                return credentials             
